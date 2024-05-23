4590f6c767484bbe00885849069afe5a3e199ece
Leon's Laptop <102eon@gmail.com>
2024-05-23 13:04:31 -0700

    fixed flashcard routing error from show -> listOne

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -5,7 +5,7 @@ const flashcardController = require('../controller/flashcard_controller');
 router.get('/flashcards', flashcardController.list);
 router.get('/flashcards/new', flashcardController.new);
 router.post('/flashcards', flashcardController.create);
-router.get('/flashcards/:id', flashcardController.show);
+router.get('/flashcards/:id', flashcardController.listOne);
 router.get('/flashcards/:id/edit', flashcardController.edit);
 router.put('/flashcards/:id', flashcardController.update);
 router.delete('/flashcards/:id', flashcardController.delete);
